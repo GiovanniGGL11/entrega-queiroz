bc0faa1aeedc361c3039631fe9ce11d5819ccff0
GiovanniGGL11 <giovannigoncalvesdelima@gmail.com>
2026-05-10 23:22:03 -0300

    Revert "style: reduzir tamanho dos ícones do sidebar de 20px para 16px"
    This reverts commit a25879daa883f609f2595be81e9c48e5c071eb3d.

--- app/layouts/dashboard.vue
+++ app/layouts/dashboard.vue
@@ -969,8 +969,8 @@ onMounted(() => {
 }
 
 .nav-link svg {
-  width: 16px;
-  height: 16px;
+  width: 20px;
+  height: 20px;
   flex-shrink: 0;
 }
 
@@ -1508,8 +1508,8 @@ onMounted(() => {
   }
   
   .nav-link svg {
-    width: 16px;
-    height: 16px;
+    width: 20px;
+    height: 20px;
   }
   
   .theme-btn, .logout-btn {
